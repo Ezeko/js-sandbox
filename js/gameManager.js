69715386cd2a9439712d29114e2c2ca6d2d4f8b7
Ezekiel ADEJOBI <ezeko2017@gmail.com>
2020-09-25 10:48:36 +0100

    add play function to game

--- js/gameManager.js
+++ js/gameManager.js
@@ -1,35 +1,38 @@
 let GameManager = {
-    setGameStart: function (name){
+    setGameStart: function (name) {
         this.resetPlayer(name);
         this.setPreFight();
     },
 
-    resetPlayer: function (name){
+    resetPlayer: function (name) {
         let playerName = name
         switch (name) {
             case 'Player One':
-                player = new Player(name, 100, 200, 150, 0 , 50)
+                player = new Player(name, 100, 200, 150, 0, 50)
                 break;
-            case 'Player Two': 
-                player = new Player(name, 80, 200, 100,0, 100);
+            case 'Player Two':
+                player = new Player(name, 80, 200, 100, 0, 100);
                 break;
-            case 'Player Three': 
-            player = new Player(name, 100, 190, 200,0, 100);
-            break;
+            case 'Player Three':
+                player = new Player(name, 100, 150, 120, 0, 100);
+                break;
         }
         event.preventDefault();
         //set page
 
-        
-        
+
+
         //set player
         let playerPage = document.querySelector("#character");
         console.log(name);
-        playerPage.innerHTML = '<img src= "/images/players/' + playerName +'.jpeg"> <div id = "players"><h3>' + name + '</h3><h5> Health: ' + player.health+'</h5><h5> Mana: '+ player.mana +'</h5><h5> Strength: '+ player.strength+'</h5><h5> Agility: '+ player.agility+ '</h5><h5> Speed: ' + player.speed+'</h5> </div> <div id ="search" onclick = "GameManager.setFight()" > Search for Enemy</div>';
+        playerPage.innerHTML = '<img src= "/images/players/' + playerName + '.jpeg"> <div id = "players"><h3>' + name + '</h3><h5 id="player-health"> Health: ' + player.health + '</h5><h5> Mana: ' + player.mana + '</h5><h5> Strength: ' + player.strength + '</h5><h5> Agility: ' + player.agility + '</h5><h5> Speed: ' + player.speed + '</h5> </div> ';
 
     },
-    setPreFight: function (){
+    setPreFight: function () {
+        let getActionField = document.querySelector('#actions')
 
+        getActionField.innerHTML = '<div onclick = "GameManager.setFight()" > Search for Enemy!</div>';
+        getActionField.style.visibility = 'visible';
         //set title
         let titlePage = document.querySelector('#title');
 
@@ -40,7 +43,35 @@ let GameManager = {
         arenaPage.innerHTML = '<img src="/images/arena.jpeg" id="arena-image">';
     },
 
-    setFight: function(){
+    setFight: function () {
+
+        let enemy00 = new Enemy('Goblin', 100, 100, 130, 0, 150);
+        let enemy01 = new Enemy('Thanos', 100, 190, 90, 0, 150)
+
+        let randNum = parseInt(Math.random().toFixed())
+        //console.log(randNum);
+        switch (randNum) {
+            case 0:
+                enemy = enemy00;
+                break;
+            case 1:
+                enemy = enemy01;
+                break;
+        }
+
+        //set title
+        let titlePage = document.querySelector('#title');
+
+        titlePage.innerHTML = '<h1> Hero Game!!</h1> <h2> Start Game</h2>';
+
+        let getActionField = document.querySelector('#actions')
+
+        getActionField.innerHTML = '<div onclick = "PlayerManagement.play()" > Attack!</div>';
+
+        let enemyPage = document.querySelector('#enemy');
+        //console.log(enemy);
+        enemyPage.innerHTML = '<img src= "/images/enemies/' + enemy.name + '.jpeg"> <div id = "enemies"><h3>' + enemy.name + '</h3><h5 id="enemy-health"> Health: ' + enemy.health + '</h5><h5> Mana: ' + enemy.mana + '</h5><h5> Strength: ' + enemy.strength + '</h5><h5> Agility: ' + enemy.agility + '</h5><h5> Speed: ' + enemy.speed + '</h5> </div>';
+
 
     }
 }
--- js/player.js
+++ js/player.js
@@ -1,10 +1,73 @@
 let player;
 
-function Player (name, health, speed, agility, mana, strength){
+function Player(name, health, speed, agility, mana, strength) {
     this.name = name;
     this.health = health;
     this.speed = speed;
     this.agility = agility;
     this.mana = mana;
     this.strength = strength
+}
+
+PlayerManagement = {
+    play: function () {
+        //determine who to play first
+        if (player.speed >= enemy.speed) {
+            let playerStats = this.playerAttack();
+
+            alert(`You hit ${playerStats[1]}, Damages made: ${playerStats[0]}`)
+            enemy.health = enemy.health - playerStats[1];
+            //if enemy is not dead continue
+            if (enemy.health > 0) {
+                let getEnemyHealth = document.querySelector('#enemy-health');
+
+                getEnemyHealth.innerHTML = `Health ${enemy.health - playerStats[1]}`;
+
+                let enemyStat = this.enemyAttack();
+                alert(`Enemy hit ${enemyStat[1]}, Damages made: ${enemyStat[0]}`)
+                player.health = player.health - enemyStat[1]
+                //if player is not dead, continue
+                if ((player.health - enemyStat[1]) > 0) {
+                    let getPlayerHealth = document.querySelector('#player-health');
+
+                    getPlayerHealth.innerHTML = `Health ${player.health - enemyStat[1]}`;
+                    
+
+                }else {
+                    let getPlayerHealth = document.querySelector('#player-health');
+
+                    getPlayerHealth.innerHTML = 'Health 0';
+
+                alert('Opps!!! You Lose!! Refresh Your browser to play again')
+                }
+            } else {
+                let getEnemyHealth = document.querySelector('#enemy-health');
+
+                getEnemyHealth.innerHTML = 'Health 0';
+
+                alert('You win!! Refresh Your browser to play again')
+            }
+
+
+        } else {
+            //enemy to play first
+        }
+
+    },
+    playerAttack: function () {
+        //this method handles all players attacking mode and returns number of hits and number of attacks
+        let numberOfFatality = (player.speed * player.agility) / 1000
+        let numberOfHits = Math.abs(Math.floor((player.health - numberOfFatality) / 10) * Math.floor(Math.random() * 10)) + 1;
+        let numberOfDamages = Math.floor(Math.random() * 10) + 1;
+        return playerAccessment = [numberOfDamages, numberOfHits];
+    },
+    enemyAttack: function () {
+        //this method handles all enemies attacking mode and returns number of hits and number of attacks
+
+        let numberOfFatality = (enemy.speed * enemy.agility) / 1000
+        let numberOfHits = Math.abs(Math.floor((player.health - numberOfFatality) / 10) * Math.floor(Math.random() * 10)) + 1;
+        let numberOfDamages = Math.floor(Math.random() * 10) + 1;
+        return enemyAccessment = [numberOfDamages, numberOfHits];
+    },
+
 }
