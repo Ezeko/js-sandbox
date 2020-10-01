ccf1e4fbb56e89d4e43d45c3bccc1a63f9764be4
Ezekiel ADEJOBI <ezeko2017@gmail.com>
2020-10-01 15:52:16 +0100

    add button for next and previous'

--- js/gallery.js
+++ js/gallery.js
@@ -18,10 +18,39 @@ allImages.forEach( function(image, index){
         newImg.setAttribute('class', 'new-img');
 
         newBg.appendChild(newImg);
+
+        //add next and prev buttons to page
+        
+        newImg.onload = ( ()=>{
+            let imgWidth = this.width;
+            console.log(windowWidth)
+            let width = ((windowWidth - imgWidth)/2) - 450 ;
+            let prevBtn = document.createElement('button');
+            newBg.appendChild(prevBtn);
+            prevBtn.appendChild(document.createTextNode('Prev'));
+            prevBtn.setAttribute('id', 'prev-btn');
+            prevBtn.style.cssText = `left: ${width}px;`;
+            prevBtn.setAttribute('onclick', "changeImage('prev')");
+
+            /**next button */
+
+            let nextBtn = document.createElement('button');
+            newBg.appendChild(nextBtn);
+            nextBtn.appendChild(document.createTextNode('Next'));
+            nextBtn.setAttribute('id', 'prev-btn');
+            nextBtn.style.cssText = `right: ${width + 145}px;`;
+            nextBtn.setAttribute('onclick', "changeImage('next')");
+
+
+        })
     })
 })
 
 
-function closeImg(){
+const closeImg = () =>{
     document.querySelector('.new-img-background').remove();
+    document.querySelector('#prev-btn').remove();
 }
+
+/** function to handle switch of image */
+
